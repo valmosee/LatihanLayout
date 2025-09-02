29fac2f5be1bb4668566badfb0ea070ce02b9746
Joice <c14230225@john.petra.ac.id>
2025-09-02 18:48:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihanlayout/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.latihanlayout
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
+
+//<LinearLayout
+//android:layout_width="match_parent"
+//android:layout_height="wrap_content">
+//<ImageView
+//android:layout_width="24dp"
+//android:layout_height="24dp">
+//
+//</ImageView>
+//</LinearLayout>
